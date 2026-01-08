213f824777fe510c2514b821d6807bcf38cb2c34
switchfl1p <switchfl1p@protonmail.com>
2026-01-08 21:18:08 +0200

    Camera class progress

--- core/Camera.cpp
+++ core/Camera.cpp
@@ -1,5 +1,6 @@
 /* switchfl1p 2025-2026 */
 
+#include <glm/ext/matrix_clip_space.hpp>
 #include <Camera.hpp>
 
 Camera::Builder& Camera::Builder::setCameraPos(glm::vec3 pos){
@@ -47,7 +48,41 @@ Camera::Builder& Camera::Builder::setPerspectiveFoV(float desired_fov){
     return *this;
 }
 
-Camera::Camera(glm::vec3 pos, glm::vec3 front, glm::vec3 up, float yaw, float pitch, float speed, float z_sens, float m_sens, float f)
+Camera::Builder& Camera::Builder::setPerspZNear(float p_zN){
+    persp_zNear_b = p_zN;
+    return *this;
+}
+
+Camera::Builder& Camera::Builder::setPerspZFar(float p_zF){
+    persp_zFar_b = p_zF;
+    return *this;
+}
+
+void Camera::updatePerspMat(){
+    perspective_mat = glm::perspective(glm::radians(fov), float(*window_width)/float(*window_height), camera_pers_zNear, camera_pers_zFar);
+}
+
+void Camera::updateDirection(){
+    direction.x = cos(glm::radians(camera_yaw)) * cos(glm::radians(camera_pitch));
+    direction.y = sin(glm::radians(camera_pitch));
+    direction.z = sin(glm::radians(camera_yaw)) * cos(glm::radians(camera_pitch));
+    camera_front = glm::normalize(direction);
+}
+
+void Camera::updateViewMat(){
+    view_mat = glm::lookAt(camera_pos, camera_pos + camera_front, camera_up);
+}
+
+glm::mat4 Camera::getPerspMat(){
+    return perspective_mat;
+}
+
+glm::mat4 Camera::getViewMat(){
+    return view_mat;
+}
+
+Camera::Camera(glm::vec3 pos, glm::vec3 front, glm::vec3 up, float yaw, float pitch,
+    float speed, float z_sens, float m_sens, float f, int* w_width, int* w_height, float p_zNear, float p_zFar)
 :   camera_pos(pos),
     camera_front(front),
     camera_up(up),
@@ -57,12 +92,18 @@ Camera::Camera(glm::vec3 pos, glm::vec3 front, glm::vec3 up, float yaw, float pi
     zoom_sens(z_sens),
     mouse_sens(m_sens),
     fov(f),
-    first_mouse(true)
+    first_mouse(true),
+    camera_pers_zNear(p_zNear),
+    camera_pers_zFar(p_zFar)
 {
-    //view_mat
-    //persp_mat
-    //direction
-    //last_x
-    //last_y
-    
+    updatePerspMat();
+    updateDirection();
+    updateViewMat();    
+}
+
+Camera::Builder::Builder(int* w, int* h) : width_b(w), height_b(h){}
+
+Camera Camera::Builder::build(){
+    return Camera(camera_pos_b, camera_front_b, camera_up_b, camera_yaw_b, camera_pitch_b, camera_speed_b,
+                 zoom_sens_b, mouse_sens_b, fov_b, width_b, height_b, persp_zNear_b, persp_zFar_b);
 }
--- core/Camera.hpp
+++ core/Camera.hpp
@@ -14,11 +14,15 @@ class Camera{
         void mouseZoomCallback(GLFWwindow* window, double x_offset, double y_offset);
 
         void updateDirection();
-        void updateView();
+        void updateViewMat();
+        void updatePerspMat();
+
         glm::mat4 getPerspMat();
+        glm::mat4 getViewMat();
 
         class Builder{
             public:
+                Builder(int* w, int* h);
                 Builder& setCameraPos(glm::vec3 pos);
                 Builder& setCameraFront(glm::vec3 front);
                 Builder& setCameraUp(glm::vec3 up);
@@ -28,6 +32,8 @@ class Camera{
                 Builder& setCameraZoomSensibility(float z_sens);
                 Builder& setCameraMouseSensibility(float m_sens);
                 Builder& setPerspectiveFoV(float desired_fov);
+                Builder& setPerspZNear(float p_zN);
+                Builder& setPerspZFar(float p_zF);
 
                 Camera build();
             private:
@@ -41,31 +47,38 @@ class Camera{
                 float zoom_sens_b = 2.0;
                 float mouse_sens_b = 0.025f;
                 float fov_b = 45.0f;
+                float persp_zNear_b = 0.1f;
+                float persp_zFar_b = 100.0f;
+
+                int* width_b;
+                int* height_b;
         };
 
     private:
-        Camera(glm::vec3 pos, glm::vec3 front, glm::vec3 up, float yaw, float pitch, float speed, float z_sens, float m_sens, float fov);
+        Camera(glm::vec3 pos, glm::vec3 front, glm::vec3 up, float yaw, float pitch, float speed, 
+            float z_sens, float m_sens, float fov, int* w_width, int* w_height, float p_zNear, float p_zFar);
+        
+        int* window_width;
+        int* window_height;
 
         glm::vec3 camera_pos;
         glm::vec3 camera_front;
         glm::vec3 camera_up;
+        glm::vec3 direction;
 
         glm::mat4 view_mat;
         glm::mat4 perspective_mat;
 
         float camera_speed;
-
-        void updateCameraSpeed(float &delta_time);
-
         float camera_yaw;
         float camera_pitch;
-        glm::vec3 direction;
-
-        float last_x;
-        float last_y;
-        bool first_mouse;
         float zoom_sens;
         float mouse_sens;
-
+        float camera_pers_zNear;
+        float camera_pers_zFar;
         float fov;
+
+        bool first_mouse;
+
+        void updateCameraSpeed(float &delta_time);
 };
